03f3d3d3504bd0fedeef554cbc132fb8817a8ccd
SidddhantDeshpande <87174561+SidddhantDeshpande@users.noreply.github.com>
2022-04-19 22:31:59 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      container:{
+      center:true,
+      padding:"",
+      screens:{
+        lg: "1124px",
+        xl: "1124px",
+        "2xl" : "1124px"
+      }
+    }
+  },
+  },
+  plugins: [],
+}
